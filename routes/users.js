7608f06f706234d613d7770b1fde1ba24e9437b3
Chris Seale <sealechris@gmail.com>
2017-04-06 12:20:51 -0500

    added registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 
 //Register
-router.get('/register', (req, res, next) =>{
-    res.send('REGISTER');
+router.post('/register', (req, res, next) =>{
+    let newUser =  new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+    User.addUser(newUser, (err, user) =>{
+        if(err){
+            res.json({sucess: false, msg: "Failed to register user."});
+        } else {
+            res.json({sucess: true, msg: "User registered."});
+        }
+    });
 });
 
 //Authenticate
@@ -16,9 +32,5 @@ router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
 
-//Validate
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
-});
 
 module.exports = router;
